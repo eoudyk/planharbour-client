331ba4cb60230bdd96ae102440240ecdb9f573d6
Emily Oudyk <eoudyk@gmail.com>
2023-10-17 13:17:05 -0400

    add basic login page and some styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LessonGeneratorPage from "./pages/LessonGeneratorPage/LessonGeneratorPage";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import LoginPage from "./pages/LoginPage/LoginPage";
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
         <div className="app">
           <Routes>
             <Route path="/" element={<LessonGeneratorPage />} />
-            {/* <Route path="/login" element={<LoginPage />} /> */}
+            <Route path="/login" element={<LoginPage />} />
             <Route path="/create" element={<LessonGeneratorPage />} />
           </Routes>
         </div>
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,2 +1,40 @@
 import { useState } from "react";
 import "../LoginPage/LoginPage.scss";
+
+function LoginPage() {
+  const handleLogin = async (e) => {
+    e.preventDefault();
+  };
+  return (
+    <>
+      <div className="login_page">
+        <form className="login-form" onSubmit={handleLogin}>
+          {/* username */}
+          <div className="login-form__username">
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              // value={username}
+              // onChange={(e) => setUserName(e.target.value)}
+              placeholder="ex: emily@emily.com"
+            />
+          </div>
+          {/* password */}
+          <div className="login-form__password">
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              // value={password}
+              // onChange={(e) => setPassword(e.target.value)}
+              placeholder="******"
+            />
+          </div>
+          <button className="login-form__button">Login</button>
+        </form>
+        <p className="signup">Signup</p>
+      </div>
+    </>
+  );
+}
+
+export default LoginPage;

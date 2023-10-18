8360cb51ebbfcfb3c2640168a4e064b1b84f6db2
Emily Oudyk <eoudyk@gmail.com>
2023-10-18 16:49:33 -0400

    move grades and subject consts into separate folder for easier reading

--- src/pages/LessonGeneratorPage/LessonGeneratorPage.jsx
+++ src/pages/LessonGeneratorPage/LessonGeneratorPage.jsx
@@ -1,36 +1,9 @@
 import { useState } from "react";
 import "../LessonGeneratorPage/LessonGenerator.scss";
 import axios from "axios";
+import { grades } from "./constants/grades";
+import { subjects } from "./constants/subjects";
 
-const grades = [
-  { display: "1", value: "Grade 1" },
-  { display: "1/2", value: "Grade 1/2 split" },
-  { display: "2", value: "Grade 2" },
-  { display: "2/3", value: "Grade 2/3 split" },
-  { display: "3", value: "Grade 3" },
-  { display: "3/4", value: "Grade 3/4 split" },
-  { display: "4", value: "Grade 4" },
-  { display: "4/5", value: "Grade 4/5 split" },
-  { display: "5", value: "Grade 5" },
-  { display: "5/6", value: "Grade 5/6 split" },
-  { display: "6", value: "Grade 6" },
-  { display: "6/7", value: "Grade 6/7 split" },
-  { display: "7", value: "Grade 7" },
-  { display: "7/8", value: "Grade 7/8 split" },
-  { display: "8", value: "Grade 8" },
-];
-const subjects = [
-  { display: "Math", value: "Math" },
-  { display: "Science", value: "Science" },
-  { display: "English Language Arts", value: "English Language Arts" },
-  { display: "Social Studies", value: "Social Studies" },
-  { display: "History", value: "History" },
-  { display: "Geography", value: "Geography" },
-  { display: "Visual Arts", value: "Visual Arts" },
-  { display: "Drama", value: "Drama" },
-  { display: "Music", value: "Music" },
-  { display: "Core French", value: "Core French" },
-];
 function LessonForm() {
   const [subtopic, setSubtopic] = useState("");
   const [lessonLength, setLessonLength] = useState("");
---
+++ src/pages/LessonGeneratorPage/constants/subjects.jsx
@@ -0,0 +1,12 @@
+export const subjects = [
+  { display: "Math", value: "Math" },
+  { display: "Science", value: "Science" },
+  { display: "English Language Arts", value: "English Language Arts" },
+  { display: "Social Studies", value: "Social Studies" },
+  { display: "History", value: "History" },
+  { display: "Geography", value: "Geography" },
+  { display: "Visual Arts", value: "Visual Arts" },
+  { display: "Drama", value: "Drama" },
+  { display: "Music", value: "Music" },
+  { display: "Core French", value: "Core French" },
+];
